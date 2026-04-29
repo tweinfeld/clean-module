37cb34f4f20c47e60fefe6e79d23ac907deed2ab
Tal Weinfeld <talw@webwise.co.il>
2026-04-29 16:31:48 +0300

    add bin entry and update file paths

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
 import clean from "./clean.js";
 import { resolve } from "node:path";
 import gui from "./gui.js";
+import process from "node:process";
 
 const basePath = resolve(process.argv[2] ?? "");
 
